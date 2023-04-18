6672b22bf34910bebc3fb28ee30930669401f069
ViktorLaishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-04-18 19:55:03 +0200

    Add files via upload

---
+++ solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        for(int i = 0; i < nums.size(); i++){
+            int complement = target - nums[i];
+            if(hash.count(complement)){
+                return {hash[complement], i};
+            }
+            hash[nums[i]] = i;
+        }
+        return {};
+    }
+};
